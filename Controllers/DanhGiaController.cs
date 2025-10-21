c2e6123efe686d11b599af3dc9fe9c19229c5e42
Rayxez <dviettai204@gmail.com>
2025-10-21 15:41:52 +0700

    Complete lien he and danh gia

---
+++ Controllers/DanhGiaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HTNLShop.Controllers
+{
+    public class DanhGiaController : Controller
+    {
+        public IActionResult DanhGia()
+        {
+            return View();
+        }
+    }
+}
